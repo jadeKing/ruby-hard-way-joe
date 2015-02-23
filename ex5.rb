1b64788ee9c073f4847b5d695d84ee99bfba8516
Joseph Knowles <joejknowles@gmail.com>
2015-02-23 11:31:48 +0000

    added conversions to ex5

--- ex5.rb
+++ ex5.rb
@@ -1,14 +1,16 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie in 2009
 height = 74 # inches
+height_in_cm = height / 0.39370
 weight = 180 # lbs
+weight_in_kg = weight / 2.2046
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height} inches or #{height_in_cm} cm tall."
+puts "He's #{weight} pounds or #{weight_in_kg} kg heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
