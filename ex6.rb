adcf87b08e96747c34afcc75dd9559ae747281fa
icicleta <icicleta@gmail.com>
2015-02-23 11:48:25 +0000

    ex6.rb added

---
+++ ex6.rb
@@ -0,0 +1,35 @@
+# number of type of people variable
+types_of_people = 10
+# variable x print a string with the variable types_of_people embedded
+x =  "There are #{types_of_people} type of people."
+# varible binary is the string binary
+binary = "binary"
+# do_not variable es the string don't
+do_not = "don't"
+# variable y is a string combined with the variables binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+
+# prints variable x
+puts x
+# prints variable y
+puts y
+
+#prints a string with variable x embedded
+puts "I said: #{x}"
+#prints a string with variable y embedded
+puts "I also said: '#{y}'."
+
+# set variable hilarious equal to false
+hilarious = false
+# set variable joke_evaluation equal to a string with variable hilarious embedded
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#prints variable joke_evaluation
+puts joke_evaluation
+
+# sets variables w and e
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# prints both variables w and e concadenaited
+puts w + e
