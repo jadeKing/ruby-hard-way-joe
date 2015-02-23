3bfa74e06c1de55d9b150905a5aeadcbe609c44d
icicleta <icicleta@gmail.com>
2015-02-23 11:18:21 +0000

    ex4_part2.rb created and add some variable and maths

--- ex4.rb
+++ ex4.rb
@@ -1,12 +1,20 @@
 # create variables
 
+# variable total number of cars
 cars = 100
-space_in_a_car = 4
+# variable number of people per car
+space_in_a_car = 4.0
+# total number of drivers
 drivers = 30
+# total number of passangers
 passengers = 90
+# number of cars with no drivers
 cars_not_driven = cars - drivers
+# total number of cars with driver
 cars_driven = drivers
+# number of space for passanger in total
 carpool_capacity = cars_driven * space_in_a_car
+#number of passangers per car
 average_passengers_per_car = passengers / cars_driven
 
 # print number os cars
---
+++ ex4_part2.rb
@@ -0,0 +1,8 @@
+# create variables
+i = 20.0
+x = 10.5
+j = 2
+
+# maths
+puts "#{i * j % x}"
+puts "#{j / i}"
