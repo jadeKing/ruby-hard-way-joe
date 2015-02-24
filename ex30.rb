deb518a090f72f7aee9bbc509a76f8d7378db7f0
Joseph Knowles <joejknowles@gmail.com>
2015-02-24 14:23:52 +0000

    add ex30.rb

---
+++ ex30.rb
@@ -0,0 +1,26 @@
+people = 90
+cars = 4
+trucks = 150000
+
+
+if cars > people && people/cars != 8.3
+  puts "We should take the cars."
+elsif cars < people
+  puts "We should not take the cars."
+else
+  puts "We can't decide."
+end
+
+if trucks > cars
+  puts "That's too many trucks."
+elsif trucks < cars
+  puts "Maybe we could take the trucks."
+else
+  puts "We still can't decide."
+end
+
+if people > trucks
+  puts "Alright, let's just take the trucks."
+else
+  puts "Fine, let's stay home then."
+end
