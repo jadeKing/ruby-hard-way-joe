60f2b183b25342aecbc74811ac95149f98ece6d5
Joseph Knowles <joejknowles@gmail.com>
2015-02-24 17:18:59 +0000

    add exercise 34

---
+++ ex34.rb
@@ -0,0 +1,11 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+print animals
+puts ""
+puts "The animal at 1 is #{animals[1]}"
+puts "The third animal is #{animals[2]}"
+puts "The first animal is #{animals[0]}"
+puts "The animal at 3 is #{animals[3]}"
+puts "The fifth animal is #{animals[4]}"
+puts "The animal at 2 is #{animals[2]}"
+puts "The sixth animal is #{animals[5]}"
+puts "The animal at 4 is #{animals[4]}"
