c738d9aad4591f55aab30b2cf8076d4acfbe57f9
Jade King <dev@jadeking.co.uk>
2015-02-25 11:33:31 +0000

    Completed ex 37

---
+++ ex37.rb
@@ -0,0 +1,34 @@
+BEGIN # rUN THIS CODE BLOCK when script is started
+END # End BLOCK
+alias #Create another name of function
+and #Same as && but lower priority
+begin end #start/end  a block 
+break #break out of a loop
+case #Different format of if statement case 1: case:2
+class # Define a new class
+def # define a method
+defined? #Check if a class function has been defined
+do #Create a that maybe takes a parameter
+else # From if else conditional statement, executed id IF or ELSIF fails
+elsif # From if else conditional statement, execited if IF fails or previous ELSIF's
+ensure #Run Whether an exception happens or not
+for # for loop. loops through count and checks conditions
+if #IF ... do this, conditional statement
+in #FOR x IN y, if x is in y 
+module #Define a new module, collection of methods
+next #Skip to the next element in the .each loop
+not #Logical not, better to use !
+or # Logical or
+redo #Rerun a code block exactly the same as the last one
+rescue # Run this code if an aexception happens
+retry #retry block, .each statement
+return #return a value/variable from a method
+self #reference the current object, class or module
+super #The parent class of this class
+then # if true then do this
+undef # Removes a function definition
+unless #Opposite of IF, unless thi happens
+until #Opposite of while, unless this happens
+when # part of case conditoinal case X; when Y;else
+while #loop, while this is true
+yield #Pause and transfer to the code block
